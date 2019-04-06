3806bae02d2b19daf0cd8a1d95904ae862e2bc0b
Gianpiero Addis <gianpiero.addis@gmail.com>
2019-04-06 11:00:10 +0200

    Query the pwnedpasswords db, add requirements

--- pwnedpass.py
+++ pwnedpass.py
@@ -1,9 +1,20 @@
 #!/usr/bin/env python3
+"""
+Check for breached passwords against the pwnedpasswords database.
+https://haveibeenpwned.com/API/v2#PwnedPasswords
+"""
 
 from getpass import getpass
 from hashlib import sha1
+from requests import get
 
 passw = getpass("Password: ")
 hashed = sha1(passw.encode("utf-8")).hexdigest().upper()
 
-print(hashed)
+lines = get("https://api.pwnedpasswords.com/range/" + hashed[:5]).text.splitlines()
+match = [l.split(":")[1] for l in lines if hashed[5:] in l]
+
+if match:
+    print("The password appears %s times in the database." % match[0])
+else:
+    print("No breaches found for your password.")
