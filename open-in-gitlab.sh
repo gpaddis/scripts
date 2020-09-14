8f17ea0c93e5ffaca050602e2bf3a80433f64c07
Gianpiero Addis <gianpiero.addis@sumcumo.com>
2020-09-14 23:07:18 +0200

    Open branch and file in Gitlab

--- open-in-gitlab.sh
+++ open-in-gitlab.sh
@@ -1,6 +1,11 @@
 #!/usr/bin/env bash
-# Open the current repository in Gitlab or Github.
+# Open the current git repository in Gitlab with the selected branch.
+# If an optional filename is passed, open that file.
+
+optional_filename=$1
 
 origin=$(git ls-remote --get-url origin)
 origin_url=$(echo $origin | sed 's%:%/%' | sed 's%git@%https://%' | sed 's/\.git//')
-open $origin_url
+branch=$(git branch --show-current)
+
+open "$origin_url/-/tree/$branch/$optional_filename"
