a1e8bea647f87ae874e1df0c51b75eab941cb7ef
Gianpiero Addis <gianpiero.addis@sumcumo.com>
2020-09-14 23:19:56 +0200

    Open files and directories according to the current position

--- open-in-gitlab.sh
+++ open-in-gitlab.sh
@@ -1,11 +1,17 @@
 #!/usr/bin/env bash
 # Open the current git repository in Gitlab with the selected branch.
-# If an optional filename is passed, open that file.
+# If an optional filename is passed, open that file from anywhere in the repo.
 
 optional_filename=$1
 
 origin=$(git ls-remote --get-url origin)
 origin_url=$(echo $origin | sed 's%:%/%' | sed 's%git@%https://%' | sed 's/\.git//')
 branch=$(git branch --show-current)
+current_location=$(git rev-parse --show-prefix)
 
-open "$origin_url/-/tree/$branch/$optional_filename"
+destination="$origin_url/-/tree/$branch/$current_location"
+if [[ $optional_filename ]]; then
+   destination+="$optional_filename"
+fi
+
+open $destination
