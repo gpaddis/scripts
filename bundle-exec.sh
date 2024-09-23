1e8c44b5f2325045406eb0b3533e6c7f0730462e
Gianpiero Addis <gianpiero.addis@sapiens.com>
2024-09-23 14:11:22 +0200

    Revert Gemfile.local

--- bundle-exec.sh
+++ bundle-exec.sh
@@ -2,8 +2,7 @@
 
 # Add a local gemfile in case there is none yet.
 # See: https://blog.widefix.com/personal-gemfile-for-development/
-test -f Gemfile.local || echo "eval_gemfile 'Gemfile'" > Gemfile.local
 
 # Run the bundle exec rails commands with a different database for each git branch.
 GIT_BRANCH_NAME=`git rev-parse --abbrev-ref HEAD`
-SILENCE_DEPRECATIONS=1 MYSQL_DATABASE=$GIT_BRANCH_NAME MYSQL_DATABASE_TEST=${GIT_BRANCH_NAME}_test BUNDLE_GEMFILE=Gemfile.local bundle exec "$@"
+SILENCE_DEPRECATIONS=1 MYSQL_DATABASE=$GIT_BRANCH_NAME MYSQL_DATABASE_TEST=${GIT_BRANCH_NAME}_test bundle exec "$@"
